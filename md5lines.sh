d35d3c7c69311d9f9c2a7534675de04c287feb2c
pcadm <pcadm@localhost.localdomain>
2017-08-18 10:35:04 +0200

    Added author and script information.

--- md5lines.sh
+++ md5lines.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Script name: md5lines.sh
+#
+# Author: P-C Markovski
+# Date (Git repo init): 2017-08-17
+# Purpose: Calculate MD5 hashes for lines in a file as well as the number of words in each line.
+
+
+
 if [[ $# -eq 0 ]]; then
 	printf "Pass a file name to calculate MD5 hashes and number of words per line..\n"
 else
